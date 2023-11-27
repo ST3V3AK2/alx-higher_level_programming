ebb2ff0e860c34c64c0b7de5289a708957abe618
Stephen <ayubastephen212@gmail.com>
2023-11-27 15:35:08 -0800

    Create c and python scripts

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: input linked list
+ *
+ * Return: 0 if there are no cycles
+ * 1 if there i a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	print_listint(list);
+	return (1);
+}
