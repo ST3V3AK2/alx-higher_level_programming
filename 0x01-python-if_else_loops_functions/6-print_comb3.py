c3a3a008a467ff60066ef85129f4ae97fb18fca6
Stephen <ayubastephen212@gmail.com>
2023-11-29 13:39:15 -0800

    Correct changes to python scriptd

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for j in range(10):
         if (i < j):
-            print(f"{i}{j}", end="")
+            print("{}{}".format(i, j), end="")
             if (i + j < 17):
                 print(", ", end="")
             else:
