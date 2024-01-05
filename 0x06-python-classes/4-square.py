647a7c11126d82ec47c7d0d241d794561b7ee223
Stephen <ayubastephen212@gmail.com>
2024-01-05 06:41:03 -0800

    correct docuentation error

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,20 +6,19 @@ class Square:
     """This creates a Square object and performs simple calculations
     on it"""
     def __init__(self, size=0):
-        """The init method initializes classes private attributes
-
-        Args:
-            size(int): the size of the square
-        Raises:
-            TypeError: if size is not an integer
-            ValueError: if size is less than 0
-        """
+        """The init method initializes classes private attributes"""
         self.__size = size
 
     @property
     def size(self):
         """
         Sets the current size of the square
+
+        Args:
+            size(int): the size of the square
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
         """
         return self.__size
 
