e2b6cdf27ed6286ea547a887ff4640977c0719ab
Stephen Ayuba <stephenayuba16@gmail.com>
2024-04-15 21:27:48 -0700

    Fix erros -6

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,7 +2,7 @@
 """Create a Rectangle Class"""
 
 
-BaseGeometry = _import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 def Rectangle(BaseGeometry):
